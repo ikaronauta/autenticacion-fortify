e0e4cb758a0ec01fca7fc35afa0772bf91728b0b
ikaronauta <jaortiz0496@misena.edu.co>
2022-01-05 10:47:14 -0500

    register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,4 +4,47 @@
     
 @section('content')
     <h1>Registro</h1>
+
+    <form action="{{route('register')}}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label><br>
+            <input type="text" name="name" id="name" autofocus placeholder="Name..." required value="{{old('name')}}">
+        </div>
+        @error('name')
+            {{$message}}
+        @enderror
+        <br>
+
+        <div class="form-group">
+            <label for="email">Email</label><br>
+            <input type="email" name="email" id="email" placeholder="Email..." required value="{{old('email')}}">
+        </div>
+        @error('email')
+            {{$message}}
+        @enderror
+        <br>
+
+        <div class="form-group">
+            <label for="password">Password</label><br>
+            <input type="password" name="password" id="password" placeholder="Ej. Abc$123!" required>
+        </div>
+        @error('password')
+            {{$message}}
+        @enderror
+        <br>
+
+        <div class="form-group">
+            <label for="password_confirmation">Password Confirmation</label><br>
+            <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Ej. Abc$123!" required>
+        </div>
+        @error('password_confirmation')
+            {{$message}}
+        @enderror
+        <br>
+
+        <div class="form-group">
+            <input type="submit" value="Send">
+        </div>
+    </form>
 @endsection
